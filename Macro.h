348b50e1b3efe12a3f7cb93292511d1826f79940
shaobao2016 <zhaoce@BJS160204-62.local>
2016-05-23 11:51:33 +0800

    测试
    这是一个测试

---
+++ Macro.h
@@ -0,0 +1,16 @@
+//
+//  Macro.h
+//  WechatLogin
+//
+//  Created by 李士杰 on 15/9/26.
+//  Copyright (c) 2015年 李士杰. All rights reserved.
+//
+
+#ifndef WechatLogin_Macro_h
+#define WechatLogin_Macro_h
+
+#define kAppid @"wx4ef6d730e2db0f4f"
+
+#define kAppSecret @"7e9e111a2bf4543ef1ddec1348bfb1aa"
+
+#endif
